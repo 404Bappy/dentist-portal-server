84f5d5f076bf7f39500a091b113fea87bacf9208
Bappy71 <19103076@iubat.edu>
2023-03-19 04:35:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+//middleware//
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfsubck.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('dentistPortal').collection('appointmentOptions');
+        const bookingsCollection = client.db('dentistPortal').collection('bookings');
+
+        //Use Aggregate to query multiple collection and then merge data
+        app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            res.send(options);
+        });
+
+        /***
+         * API Naming Convention
+         * app.get('/bookings')
+         * app.get(/bookings/:id)
+         * app.post('/bookings')
+         * app.patch('/bookings/:id')
+         * app.delete('/bookings/:id')
+         */
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
+app.get('/', async (req, res) => {
+    res.send('dentist-portal server is running');
+})
+
+app.listen(port, () => console.log(`Dentist Portal running on ${port}`));
